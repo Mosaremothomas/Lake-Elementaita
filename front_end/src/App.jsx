a9304a3eb5cffb544e897c049fc71c45dc12302c
alexwambui <alexwambuik@gmail.com>
2023-04-26 19:45:47 +0300

    added a scroll to top function that fixes scrolling to top of page on changing the route

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Homepage from './containers/Homepage'
 import Weddings from './containers/Weddings'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
+      <BrowserRouter>        
+        <ScrollToTop />
+        <Routes>          
           <Route path="/" element={ <Homepage /> } />
           <Route path="/weddings" element={ <Weddings /> } />
         </Routes>

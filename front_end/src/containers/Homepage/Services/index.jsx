9b1fac7084710478dc3603cb6cba016d19b51881
alexwambui <alexwambuik@gmail.com>
2023-04-26 03:26:41 +0300

    added the weddings service that links to a new route - weddings

--- front_end/src/containers/Homepage/Services/index.jsx
+++ front_end/src/containers/Homepage/Services/index.jsx
@@ -1,17 +1,19 @@
+import { Link } from 'react-router-dom'
+
 import './services.css'
 
 import big5_lounge from '../../../assets/big5-lounge.png'
 import sweeties_restaurant from '../../../assets/sweeties-restaurant.png'
-import lovers_garden from '../../../assets/lovers_garden.png'
+import lovers_garden from '../../../assets/lovers_garden_.png'
 
 export default function Services() {
   return (
     <section className="Services" id="Services">
         <h1>Services</h1>
         <div className="services_wrapper">
-            <div class="service">
+            <div className="service">
                 <img src={big5_lounge} alt="" />
-                <div class="description">
+                <div className="description">
                     <h4>Big 5 Lounge</h4>
                     <p>Has a long grand dining table for upto 20 pax tuned to your liking.</p>
                 </div>
@@ -19,18 +21,28 @@ export default function Services() {
 
             <div className="service">
                 <img src={sweeties_restaurant} alt="" />
-                <div class="description">
-                    <h4>Sweetie Restaurant</h4>
+                <div className="description">
+                    <h4>Sweetie's Restaurant</h4>
                     <p>Enjoy the outdoor buffet breakfast at the sweetie restaurant.</p>
                 </div>
             </div>
 
-            <div class="service">
+            <div className="service">
                 <img src={lovers_garden} alt="" />
-                <div class="description">
+                <div className="description">
                     <h4>Lover's Garden</h4>
                     <p>Enjoy quite moments with your loved ones at the lush green gardens.</p>
                 </div>
+            </div>
+        </div>
+
+        <div className="weddings">
+            <img src={ sweeties_restaurant } alt="" />
+            <div className="weddings_info">
+                <h1>Weddings</h1>
+                <p>Lake Elementaita Manor Weddings is a once-in-a-lifetime experience and a wonderful adventure, not just for the bride and groom, but for everyone involved.</p>
+                <p>When you combine the allure of travel, a remarkable location, and an extraordinary cause for celebration, the mix is rich with meaning, memories and opportunities for bonding.</p>
+                <Link to="/weddings">Discover More</Link>
             </div>
         </div>
     </section>

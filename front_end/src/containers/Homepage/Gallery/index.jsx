bf5d73478c139756916d661d006d218631f4106d
alexwambui <alexwambuik@gmail.com>
2023-04-26 03:22:59 +0300

    added new images to the gallery

--- front_end/src/containers/Homepage/Gallery/index.jsx
+++ front_end/src/containers/Homepage/Gallery/index.jsx
@@ -6,6 +6,9 @@ import entrance02 from '../../../assets/entrance02.png'
 import screen from '../../../assets/screen.png'
 import lovers from '../../../assets/lovers_garden.png'
 import wine_glass from '../../../assets/wine_glass.png'
+import horse_ride from '../../../assets/horse_ride.png'
+import meal from '../../../assets/meal.png'
+import sunset from '../../../assets/sunset.png'
 
 export default function Gallery() {
     return (
@@ -22,6 +25,18 @@ export default function Gallery() {
 
                 <div className="gallery">
                     <img src={entrance02} alt="" />
+                </div>                
+
+                <div className="gallery">
+                    <img src={lovers} alt="" />
+                </div>
+
+                <div className="gallery">
+                    <img src={ meal } alt="" />
+                </div>
+
+                <div className="gallery">
+                    <img src={wine_glass} alt="" />
                 </div>
 
                 <div className="gallery">
@@ -29,11 +44,11 @@ export default function Gallery() {
                 </div>
 
                 <div className="gallery">
-                    <img src={lovers} alt="" />
-                </div>
+                    <img src={ horse_ride } alt="" />
+                </div>                
 
                 <div className="gallery">
-                    <img src={wine_glass} alt="" />
+                    <img src={ sunset } alt="" />
                 </div>
             </div>
         </section>
